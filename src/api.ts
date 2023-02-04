115146f4fd758c459954ff8d576f101f5db04d30
Infinity Intellect <snavaneetharan@gmail.com>
2023-02-05 00:51:33 +0530

    Change BASE_URL to https

--- src/api.ts
+++ src/api.ts
@@ -2,7 +2,7 @@ import { MovieDetail } from "./models/movies";
 import { Default, SearchResponse } from "./models/responses";
 
 const API_KEY = process.env.REACT_APP_API_KEY || "please_provide_api_key"
-const BASE_URL = `http://www.omdbapi.com/?apikey=${API_KEY}`
+const BASE_URL = `https://www.omdbapi.com/?apikey=${API_KEY}`
 
 export const getMoviesByTitle = (title: string, page: number): Promise<Default & SearchResponse> =>{
   return fetch(`${BASE_URL}&s=${title}&page=${page}&type=movie`).then((response) => {
